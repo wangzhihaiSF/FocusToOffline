edafd1ea875f2660d28221867c97da43c7f9d8fc
wangzhihaiSF <1599315665@qq.com>
2018-05-17 15:42:40 +0800

    added function to modify user's image by take photo

--- app/src/main/java/com/wangzhihai/focustooffline/LoginActivity.java
+++ app/src/main/java/com/wangzhihai/focustooffline/LoginActivity.java
@@ -2,11 +2,19 @@
 package com.wangzhihai.focustooffline;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -14,7 +22,11 @@ public class LoginActivity extends BasicActivity {
 
     private EditText account;
     private EditText password;
+
     private CircleImageView userImage;
+    private Uri imageUri;
+
+    private static final int TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,34 @@ public class LoginActivity extends BasicActivity {
         account = findViewById(R.id.et_account);
         password = findViewById(R.id.et_password);
         Button btnLogin = findViewById(R.id.btn_login);
+
+        userImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //创建 file 对象，用于储存拍照后的图片
+                File outputImage = new File(getExternalCacheDir(), "outputImage.jpg");
+                try {
+                    if (outputImage.exists()) {
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUri = FileProvider.getUriForFile(LoginActivity.this, "com.wangzhihai." +
+                            "fileprovider", outputImage);
+                }else {
+                    imageUri = Uri.fromFile(outputImage);
+                }
+                //启动相机
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+
+
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +81,26 @@ public class LoginActivity extends BasicActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        //将怕死和的照片显示出来
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                                .openInputStream(imageUri));
+                        userImage.setImageBitmap(bitmap);
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
